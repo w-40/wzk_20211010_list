4e3f1e4cbca70b4761988bbb0a54817ab07b29ee
zk <2568441388@qq.com>
2021-10-12 23:28:16 +0800

    线性表1.0

---
+++ src/nuc/sw/list/sqlist/MyArrayList.java
@@ -0,0 +1,130 @@
+package nuc.sw.list.sqlist;
+
+import java.util.Scanner;
+
+/**
+ * @author wzk
+ * @version 1.0
+ * @description 课程融合之线性表（顺序线性表）
+ * 定义一个类MyArrayList，用数组模拟实现数据结构中线性表的基本功能，该类中最少包含的属性有：data（String[]）、size（int）、CAPACITY（int）;
+ * 该类中方法最少包括线性表的初始化、插入 、查找、遍历线性表、删除、返回线性表长度、线性表判空、清空线性表、替换、获取元素、判断线性表中是否包含某个元素、反向查找、末尾插入元素；
+ * 定义一个主类，在主方法中创建线性表类对象并调用上述方法、完成功能演示。
+ */
+public class MyArrayList {
+    private String[] data;
+    private final int CAPACITY = 100;        //初始空间分配的大小
+    private int size = 0;      //实际大小
+    Scanner sc = new Scanner(System.in);
+
+    // 1.初始化
+    public MyArrayList() {
+        data = new String[CAPACITY];
+    }
+
+    // 2.初始化
+    public MyArrayList(String[] data) {
+        this.data = data;
+    }
+
+    //3.添加元素（末尾）
+    public void add(String s) {
+        // 判断线性表size--扩容
+        if (size == CAPACITY - 1) {
+            String[] newData = new String[data.length + 10];
+            data = newData;
+        }
+        add(size, s);
+    }
+
+    // 4. 添加元素
+    public void add(int index, String s) {
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = s;
+        size++;
+    }
+
+    // 5.遍历
+    public void printAll() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(data[i] + " ");
+        }
+    }
+
+    // 6.查询元素
+    public void searchData() {
+        System.out.println("请输入要查询的元素：");
+        String element = sc.next();
+
+        for (int i = 0; i < size; i++) {
+            if (element.equals(data[i])) {
+                int index = i + 1;
+                System.out.println("查询成功，该元素在线性表的第" + index + "个索引位置(从1开始)");
+                return;
+            }
+        }
+        System.out.println("未查询到该元素");
+    }
+
+    public void deleteData() {
+        System.out.println("请输入要删除的元素的位置(从1开始)：");
+        int index = sc.nextInt();
+        index--;
+        String[] newData = new String[data.length - 1];
+        for (int i = 0; i < size; i++) {
+            if (i < index) {
+                newData[i] = data[i];
+            } else {
+                newData[i] = data[i + 1];
+            }
+        }
+        data = newData;
+        size--;
+        System.out.println("删除成功");
+    }
+
+    public int getLength() {
+        return size;
+    }
+
+    public void isEmpty() {
+        if (data != null) {
+            System.out.println("线性表不为空");
+        } else {
+            System.out.println("线性表为空");
+        }
+    }
+
+    public void clearArray() {
+        data = null;
+    }
+
+    public void isData() {
+        System.out.println("请输入要判断的元素：");
+        String element = sc.next();
+
+        for (int i = 0; i < size; i++) {
+            if (element.equals(data[i])) {
+                System.out.println("线性表中包含该元素");
+                return;
+            }
+        }
+        System.out.println("线性表中不包含该元素");
+    }
+
+    public void reverseArrayList() {
+        System.out.println("请输入要查询的元素：");
+        String element = sc.next();
+
+        for (int i = size - 1; i >= 0; i--) {
+            if (element.equals(data[i])) {
+                int index = i + 1;
+                int lastIndex = size - index + 1;
+                System.out.println("查询成功，该元素在线性表的倒数第" + lastIndex + "个位置");
+                return;
+            }
+        }
+        System.out.println("未查询到该元素");
+    }
+}
---
+++ src/nuc/sw/list/sqlist/TestMyArrayList.java
@@ -0,0 +1,73 @@
+package nuc.sw.list.sqlist;
+
+import java.util.Scanner;
+
+public class TestMyArrayList {
+    public static void main(String[] args) {
+        MyArrayList myList = new MyArrayList();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("--------课程融合之线性表--------");
+            System.out.println("1 线性表的初始化");
+            System.out.println("2 插入元素");
+            System.out.println("3 查找元素");
+            System.out.println("4 遍历线性表");
+            System.out.println("5 删除元素");
+            System.out.println("6 返回线性表长度");
+            System.out.println("7 线性表判空");
+            System.out.println("8 清空线性表");
+            System.out.println("9 判断线性表中是否包含某个元素");
+            System.out.println("10 末尾插入元素");
+            System.out.println("11 反向查找");
+            System.out.println("请输入您的选择");
+
+            String choice = sc.next();
+
+            switch (choice) {
+                case "1":
+                    System.out.println("初始化成功！");
+                    break;
+                case "2":
+                    System.out.println("请输入要插入的元素的位置(从1开始)：");
+                    int index = sc.nextInt();
+                    index--;
+                    System.out.println("请输入要插入的元素的值");
+                    String value = sc.next();
+                    myList.add(index,value);
+                    break;
+                case "3":
+                    myList.searchData();
+                    break;
+                case "4":
+                    myList.printAll();
+                    break;
+                case "5":
+                    myList.deleteData();
+                    break;
+                case "6":
+                    System.out.println("线性表长度为：" + myList.getLength());
+                    break;
+                case "7":
+                    myList.isEmpty();
+                    break;
+                case "8":
+                    myList.clearArray();
+                    break;
+                case "9":
+                    myList.isData();
+                    break;
+                case "10":
+                    System.out.println("请输入要插入的元素的值");
+                    value = sc.next();
+                    myList.add(value);
+                    break;
+                case "11":
+                    myList.reverseArrayList();
+                    break;
+                default:
+                    System.out.println("您的输入有误");
+            }
+        }
+    }
+}
